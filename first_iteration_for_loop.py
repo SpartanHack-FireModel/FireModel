d335717146bee19a17df65f5e158821306f96b24
Blake LaFuente <blakelafuente@gmail.com>
2018-01-20 18:16:31 -0500

    cleanup

--- first_iteration_for_loop.py
+++ first_iteration_for_loop.py
@@ -185,7 +185,7 @@ def set_board(burnable):
             game_board[x,y].humidity = humidity
             game_board[x,y].elevation = elevation
             game_board[x,y].crowning, game_board[x,y].transition = crowntransit(game_board[x,y].biome)
-
+            game_board[x,y].conditions = getConditions(game_board[x,y].crowning, game_board[x,y].transition)
 
     return game_board
 
