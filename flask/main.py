6ee53cbacf61b9a673b116da1905bce0afb22f3c
Bradley <mustyoshi@gmail.com>
2018-01-20 17:56:11 -0500

    Flask server

---
+++ flask/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, send_file
+from flask_cors import CORS
+import json
+import os.path
+app = Flask(__name__)
+CORS(app)
+@app.route('/startsimulation',methods=["POST"])
+def hello_world():
+  args = {'Args' : request.get_json(),
+  'Frames' : 2,
+  'Subdir' : 'example'
+  }
+  return json.dumps(args)
+
+@app.route('/imgs/<path:sess>/<path:path>')
+def imgResp(sess,path):
+  url = './imgs/' + sess + '/' + path
+  print('Looking for',url)
+  if(os.path.isfile(url)):
+    print('Found')
+    return send_file(url,mimetype="image/png")
+  else:
+    return ''
